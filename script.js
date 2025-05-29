582db693ac76ff965eb5e382d8738f1dd5863e22
ThinkingTechDev <admin@thinkingtech.dev>
2025-05-29 13:59:53 +0800

    changed buttons to string

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-const buttons = [
-    "7",
-    "8",
-    "9",
-    "/",
-    "4",
-    "5",
-    "6",
-    "*",
-    "1",
-    "2",
-    "3",
-    "-",
-    "0",
-    "C",
-    "=",
-    "+",
-];
+const buttons = "7,8,9,/,4,5,6,*,1,2,3,-,0,C,=,+".split(",");
 
 const display = document.getElementById("display");
 const calculator = document.querySelector(".calculator");
